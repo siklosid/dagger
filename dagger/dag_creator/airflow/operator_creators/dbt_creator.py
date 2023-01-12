a61d305182eee91a7dea38cabb1aef14bef35e44
Jorge Tagle <jorge.tagle@choco.com>
2023-01-12 18:02:34 +0100

    Refactor dbt creator extending the batch creator

--- dagger/dag_creator/airflow/operator_creators/dbt_creator.py
+++ dagger/dag_creator/airflow/operator_creators/dbt_creator.py
@@ -1,8 +1,8 @@
-from dagger.dag_creator.airflow.operator_creator import OperatorCreator
-from airflow.operators.bash import BashOperator
+from dagger.dag_creator.airflow.operator_creators.batch_creator import BatchCreator
 import json
 
-class DbtCreator(OperatorCreator):
+
+class DbtCreator(BatchCreator):
     ref_name = "dbt"
 
     def __init__(self, task, dag):
@@ -13,47 +13,11 @@ class DbtCreator(OperatorCreator):
         self._profile_name = task.profile_name
         self._select = task.select
 
-    def _generate_deps_command(self):
-        command = [
-            "dbt deps",
-            f"--project-dir {self._project_dir}",
-            f"--profiles-dir {self._profile_dir}",
-        ]
-
-        return " ".join(command)
-
-    def _generate_build_command(self):
-        command = [
-            "dbt build",
-            f"--project-dir {self._project_dir}",
-            f"--profiles-dir {self._profile_dir}",
-            f"--target {self._profile_name}",
-        ]
-
-        if self._select:
-            command += [f"--select {self._select}"]
+    def _generate_command(self):
+        command = super()._generate_command()
 
         if len(self._template_parameters) > 0:
             dbt_vars = json.dumps(self._template_parameters)
-            command += [f"--vars '{dbt_vars}'"]
+            command.append(f"--vars='{dbt_vars}'")
 
-        return " ".join(command)
-
-    def _create_operator(self, **kwargs):
-        full_bash_command = f"{self._generate_deps_command()}; {self._generate_build_command()}"
-
-        dbt_op = BashOperator(
-            dag=self._dag,
-            task_id=self._task.name,
-            bash_command=full_bash_command,
-            **kwargs
-        )
-
-        return dbt_op
-
-    # Overwriting function because for dbt we don't want to add inputs/outputs to the template parameters
-    def create_operator(self):
-        self._template_parameters.update(self._task.template_parameters)
-        self._update_airflow_parameters()
-
-        return self._create_operator(**self._airflow_parameters)
+        return command
