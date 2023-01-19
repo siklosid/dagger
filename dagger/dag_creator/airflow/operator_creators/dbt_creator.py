e67bdb980d760febfe675f2ea4b35b56e4bcd12d
Jorge Tagle <jorge.tagle@choco.com>
2023-01-19 18:50:17 +0100

    Reintroduce previously removed command generator

--- dagger/dag_creator/airflow/operator_creators/dbt_creator.py
+++ dagger/dag_creator/airflow/operator_creators/dbt_creator.py
@@ -14,10 +14,22 @@ class DbtCreator(BatchCreator):
         self._select = task.select
 
     def _generate_command(self):
-        command = super()._generate_command()
+        command = [self._task.executable_prefix, self._task.executable]
+        command.append(f"--project-dir {self._project_dir}")
+        command.append(f"--profiles-dir {self._profile_dir}")
+        command.append(f"--target {self._profile_name}")
+        if self._select:
+            command.append(f"--select {self._select}")
 
         if len(self._template_parameters) > 0:
             dbt_vars = json.dumps(self._template_parameters)
             command.append(f"--vars='{dbt_vars}'")
 
         return command
+
+    # Overwriting function because for dbt we don't want to add inputs/outputs to the template parameters
+    def create_operator(self):
+        self._template_parameters.update(self._task.template_parameters)
+        self._update_airflow_parameters()
+
+        return self._create_operator(**self._airflow_parameters)
