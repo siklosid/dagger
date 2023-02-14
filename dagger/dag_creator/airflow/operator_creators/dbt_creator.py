2a2c6efcff90fead7c0005b82f1c9fe177a45cfd
JeanMellot1995 <72747580+JeanMellot1995@users.noreply.github.com>
2023-02-14 09:31:11 +0100

    align dbt_task with dbt_creator

--- dagger/dag_creator/airflow/operator_creators/dbt_creator.py
+++ dagger/dag_creator/airflow/operator_creators/dbt_creator.py
@@ -14,14 +14,14 @@ class DbtCreator(BatchCreator):
         self._profile_dir = task.profile_dir
         self._profile_name = task.profile_name
         self._select = task.select
-        self._command = task.command
+        self._dbt_command = task.dbt_command
 
     def _generate_command(self):
         command = [self._task.executable_prefix, self._task.executable]
         command.append(f"--project_dir={self._project_dir}")
         command.append(f"--profiles_dir={self._profile_dir}")
         command.append(f"--profile_name={self._profile_name}")
-        command.append(f"--command={self._command}")
+        command.append(f"--dbt_command={self._dbt_command}")
         if self._select:
             command.append(f"--select={self._select}")
 
