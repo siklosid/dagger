77fb2b24b4d83bb2b8c742daf4ae5b6c5be4a7d3
Kiran Vasudev <kiran.vasudev@outlook.com>
2023-11-16 11:51:09 +0100

    setUp test class

---
+++ tests/utilities/test_dbt_config_parser.py
@@ -0,0 +1,30 @@
+import logging
+import unittest
+from unittest import skip
+from unittest.mock import patch, MagicMock
+
+from dagger.utilities.dbt_config_parser import DBTConfigParser
+from dagger.utilities.module import Module
+from tests.fixtures.modules.dbt_config_parser_fixtures import (
+    EXPECTED_DBT_MODEL_PARENTS,
+    EXPECTED_DAGGER_INPUTS,
+    DBT_MANIFEST_FILE_FIXTURE,
+    DBT_PROFILE_FIXTURE,
+    EXPECTED_DAGGER_OUTPUTS,
+)
+
+_logger = logging.getLogger("root")
+
+DEFAULT_CONFIG_PARAMS = {
+    "data_bucket": "bucket1-data-lake",
+    "project_dir": "main",
+    "profile_dir": ".dbt",
+}
+
+
+class TestDBTConfigParser(unittest.TestCase):
+    @patch("builtins.open", new_callable=MagicMock, read_data=DBT_MANIFEST_FILE_FIXTURE)
+    @patch("json.loads", return_value=DBT_MANIFEST_FILE_FIXTURE)
+    @patch("yaml.safe_load", return_value=DBT_PROFILE_FIXTURE)
+    def setUp(self, mock_open, mock_json_load, mock_safe_load):
+        self._dbt_config_parser = DBTConfigParser(DEFAULT_CONFIG_PARAMS)
